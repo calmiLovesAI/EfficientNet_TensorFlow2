402b54315e5d505902b4e2e129cf5b52312206b7
calmisential <xinyu_std@163.com>
2019-09-27 17:26:37 +0800

    Upload EfficientNet

---
+++ efficientnet.py
@@ -0,0 +1,132 @@
+import tensorflow as tf
+
+NUM_CLASSES = 10
+
+
+class SEBlock(tf.keras.layers.Layer):
+    def __init__(self, input_channels, r=4):
+        super(SEBlock, self).__init__()
+        self.pool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc1 = tf.keras.layers.Dense(units=input_channels // r)
+        self.fc2 = tf.keras.layers.Dense(units=input_channels)
+
+    def call(self, inputs, **kwargs):
+        branch = self.pool(inputs)
+        branch = self.fc1(branch)
+        branch = tf.nn.relu(branch)
+        branch = self.fc2(branch)
+        branch = tf.nn.sigmoid(branch)
+        branch = tf.expand_dims(input=branch, axis=1)
+        branch = tf.expand_dims(input=branch, axis=1)
+        output = inputs * branch
+        return output
+
+
+class MBConv(tf.keras.layers.Layer):
+    def __init__(self, in_channels, out_channels, expansion_factor, stride, k):
+        super(MBConv, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = stride
+        self.conv1 = tf.keras.layers.Conv2D(filters=in_channels * expansion_factor,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding="same")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.dwconv = tf.keras.layers.DepthwiseConv2D(kernel_size=(k, k),
+                                                      strides=stride,
+                                                      padding="same")
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.se = SEBlock(input_channels=in_channels * expansion_factor)
+        self.conv2 = tf.keras.layers.Conv2D(filters=out_channels,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding="same")
+        self.bn3 = tf.keras.layers.BatchNormalization()
+
+    def call(self, inputs, training=None, **kwargs):
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu6(x)
+        x = self.dwconv(x)
+        x = self.bn2(x, training=training)
+        x = self.se(x)
+        x = tf.nn.relu6(x)
+        x = self.conv2(x)
+        x = self.bn3(x, training=training)
+        if self.stride == 1 and self.in_channels == self.out_channels:
+            x = tf.keras.layers.add([x, inputs])
+        return x
+
+
+def build_mbconv_block(in_channels, out_channels, layers, stride, expansion_factor, k):
+    block = tf.keras.Sequential()
+    for i in range(layers):
+        if i == 0:
+            block.add(MBConv(in_channels=in_channels,
+                             out_channels=out_channels,
+                             expansion_factor=expansion_factor,
+                             stride=stride,
+                             k=k))
+        else:
+            block.add(MBConv(in_channels=out_channels,
+                             out_channels=out_channels,
+                             expansion_factor=expansion_factor,
+                             stride=1,
+                             k=k))
+    return block
+
+
+class EfficientNet(tf.keras.Model):
+    def __init__(self):
+        super(EfficientNet, self).__init__()
+        self.conv1 = tf.keras.layers.Conv2D(filters=32,
+                                            kernel_size=(3, 3),
+                                            strides=2,
+                                            padding="same")
+        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.block1 = build_mbconv_block(in_channels=32, out_channels=16, layers=1, stride=1,
+                                         expansion_factor=1, k=3)
+        self.block2 = build_mbconv_block(in_channels=16, out_channels=24, layers=2, stride=2,
+                                         expansion_factor=6, k=3)
+        self.block3 = build_mbconv_block(in_channels=24, out_channels=40, layers=2, stride=2,
+                                         expansion_factor=6, k=5)
+        self.block4 = build_mbconv_block(in_channels=40, out_channels=80, layers=3, stride=2,
+                                         expansion_factor=6, k=3)
+        self.block5 = build_mbconv_block(in_channels=80, out_channels=112, layers=3, stride=1,
+                                         expansion_factor=6, k=5)
+        self.block6 = build_mbconv_block(in_channels=112, out_channels=192, layers=4, stride=2,
+                                         expansion_factor=6, k=5)
+        self.block7 = build_mbconv_block(in_channels=192, out_channels=320, layers=1, stride=1,
+                                         expansion_factor=6, k=3)
+
+        self.conv2 = tf.keras.layers.Conv2D(filters=1280,
+                                            kernel_size=(1, 1),
+                                            strides=1,
+                                            padding="same")
+        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.pool = tf.keras.layers.GlobalAveragePooling2D()
+        self.fc = tf.keras.layers.Dense(units=NUM_CLASSES,
+                                        activation=tf.keras.activations.softmax)
+
+    def call(self, inputs, training=None, mask=None):
+        x = self.conv1(inputs)
+        x = self.bn1(x, training=training)
+        x = tf.nn.relu6(x)
+
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.block4(x)
+        x = self.block5(x)
+        x = self.block6(x)
+        x = self.block7(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x, training=training)
+        x = tf.nn.relu6(x)
+        x = self.pool(x)
+        x = self.fc(x)
+
+        return x
+
